728c0d73a0d5d7fb77e054615f4a671efdf818e3
dmadust@gmail.com <dmadust@gmail.com>
2018-04-22 19:39:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector); 
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  var elements = document.querySelectorAll(".ranked-list li");
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].innerHTML = (parseInt(elements[i].innerHTML, 10) + n).toString();
+  }
+}
+
+function deepestChild() {
+  var deepest = document.querySelector("#grand-node");
+  while (deepest.querySelector("div") != undefined) {
+    deepest = deepest.querySelector("div");
+  }
+  return deepest;
+}
