793f86867dafc431a9c19520f8e56150239c8dad
dmadust@gmail.com <dmadust@gmail.com>
2018-04-22 19:33:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   var elements = document.querySelectorAll(".ranked-list li");
-  console.log(elements.length);
   for (let i = 0; i < elements.length; i++) {
     elements[i].innerHTML = (parseInt(elements[i].innerHTML, 10) + n).toString();
   }
